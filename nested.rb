ba5e21fddf36898968f77d3f6143b922b5d70186
Sean LaFlam <sean.laflam@gmail.com>
2020-08-17 21:15:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -38,6 +38,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+programmer_hash[:alan_kay][:known_for]
 
 end
 
@@ -57,7 +58,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -83,7 +84,7 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:]
 end
 
 def changing_alan
