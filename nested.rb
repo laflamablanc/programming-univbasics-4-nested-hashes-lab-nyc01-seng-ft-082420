f11753e54a4e29f6475f640aba40ba02e44f3a83
Sean LaFlam <sean.laflam@gmail.com>
2020-08-17 21:16:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,8 +84,11 @@ def adding_matz
         :languages => ["C"]
       }
     }
-  programmer_hash[:]
+  programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby", languages: ["LISP","C"]}
+  
+  programmer_hash
 end
+
 
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
